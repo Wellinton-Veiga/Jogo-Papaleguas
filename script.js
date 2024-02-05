d8a16fea3ef49e331a6dd285dd325373f94a21e1
Wellinton Veiga <lipbueno@hotmail.com>
2024-02-05 20:11:27 -0300

    Funções do Coyote e Papaleguas

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const papaleguas = document.querySelector('.papaleguas');
+const coyote = document.querySelector('.coyote');
+
 
 const jump = () => {
     papaleguas.classList.add('jump');
@@ -8,5 +10,21 @@ const jump = () => {
 
     }, 500);
 }
+
+const loop = setInterval(() => {
+
+    const coyotePosition = coyote.offsetLeft;
+    const papaleguasPosition = +window.getComputedStyle(papaleguas).bottom.replace('px',' ');
+
+    if (coyotePosition <= 100 && coyotePosition > 0 && papaleguasPosition < 80) {
+
+        coyote.style.animation = 'none';
+        coyote.style.left = `${coyotePosition}px`;
+
+        papaleguas.style.animation = 'none';
+        papaleguas.style.bottom = `${papaleguasPosition}px`;
+    }
+    
+}, 10);
 
 document.addEventListener('keydown', jump);
